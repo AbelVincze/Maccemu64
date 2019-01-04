e7a6044c07abf969cbefd2574f84b1428bf76987
AbelVincze <abel@iparigrafika.hu>
2019-01-04 21:53:37 +0100

    Initial Commit

---
+++ Maccemu64/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Maccemu64
+//
+//  Created by Macc on 19/01/04.
+//  Copyright © 2019. Macc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Maccemu64/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Maccemu64
+//
+//  Created by Macc on 19/01/04.
+//  Copyright © 2019. Macc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
