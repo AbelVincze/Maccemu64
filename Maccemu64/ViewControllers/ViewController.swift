ec65b8cda9eae1412f44fa9aed20e5951e770b45
AbelVincze <abel@iparigrafika.hu>
2019-01-05 00:54:55 +0100

    inital setup

--- Maccemu64/ViewControllers/ViewController.swift
+++ Maccemu64/ViewControllers/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        print( "maccemu64 main view loaded...")
     }
 
     override var representedObject: Any? {
